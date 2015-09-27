e8a9b7c8fb144295a832bea6b3a9f071a7f275f6
Manu Phatak <bionikspoon@gmail.com>
2015-09-27 02:07:32 -0500

    user model unittests

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from flask import current_app
 from flask.ext.login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db, login_manager
@@ -19,11 +21,13 @@ class Role(db.Model):
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -38,6 +42,22 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
 
 @login_manager.user_loader
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,30 +1,100 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import time
 import unittest
+
+from app import create_app, db
 from app.models import User
 
 
+# noinspection PyArgumentList
 class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.user = User(password='secret')
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
     def test_password_setter(self):
-        user = User(password='secret')
-        self.assertTrue(user.password_hash is not None)
+        self.assertTrue(self.user.password_hash is not None)
 
     def test_no_password_getter(self):
-        user = User(password='secret')
         with self.assertRaises(AttributeError):
             # noinspection PyStatementEffect
-            user.password
+            self.user.password
 
     def test_password_verification(self):
-        user = User(password='secret')
-        self.assertTrue(user.verify_password('secret'))
-        self.assertFalse(user.verify_password('not_secret'))
+        self.assertTrue(self.user.verify_password('secret'))
+        self.assertFalse(self.user.verify_password('not_secret'))
 
     def test_password_salts_are_random(self):
         user1 = User(password='secret')
         user2 = User(password='secret')
         self.assertNotEqual(user1.password_hash, user2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        db.session.add(self.user)
+        db.session.commit()
+        token = self.user.generate_confirmation_token()
+        self.assertTrue(self.user.confirm(token))
+
+    def test_invalid_confirmation_token(self):
+        user1 = User(password='secret')
+        user2 = User(password='secret')
+        db.session.add_all([user1, user2])
+        db.session.commit()
+
+        token = user1.generate_confirmation_token()
+        self.assertFalse(user2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        db.session.add(self.user)
+        db.session.commit()
+        token = self.user.generate_confirmation_token(1)
+        time.sleep(2)
+        self.assertFalse(self.user.confirm(token))
+
+    def test_valid_reset_token(self):
+        db.session.add(self.user)
+        db.session.commit()
+
+        # token = self.user.generate_confirmation_token()
+        # self.assertTrue(self.user.confirm(token))
+
+    def test_invalid_reset_token(self):
+        user1 = User(password='secret')
+        user2 = User(password='secret')
+        db.session.add_all([user1, user2])
+        db.session.commit()
+
+        # token = user1.generate_confirmation_token()
+        # self.assertFalse(user2.confirm(token))
+
+    def test_valid_email_change_token(self):
+        db.session.add(self.user)
+        db.session.commit()
+
+        # token = self.user.generate_confirmation_token()
+        # self.assertTrue(self.user.confirm(token))
+
+    def test_invalid_email_change_token(self):
+        user1 = User(password='secret')
+        user2 = User(password='secret')
+        db.session.add_all([user1, user2])
+        db.session.commit()
+
+        # token = user1.generate_confirmation_token()
+        # self.assertFalse(user2.confirm(token))
+
+    def test_duplicate_email_change_token(self):
+        pass
 
 
 if __name__ == '__main__':
