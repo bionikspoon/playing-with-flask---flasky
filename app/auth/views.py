2b1c7cdda8bf8a62f129727a610de4f55888812f
Manu Phatak <bionikspoon@gmail.com>
2015-09-27 00:17:54 -0500

    user logout feature

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user
+from flask.ext.login import login_user, login_required, logout_user
 
 from . import auth
 from .forms import LoginForm
@@ -16,11 +16,15 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
+            flash('You have been logged in. Welcome back %s!' % user.username)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
