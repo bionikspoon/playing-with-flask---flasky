92286f20b2e0a1d9382376719b378df28814bb0a
Manu Phatak <bionikspoon@gmail.com>
2015-09-27 17:48:14 -0500

    catch unconfirmed users

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,6 @@
 
 from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, login_required, logout_user, current_user
-
 from app.email import send_email
 from . import auth
 from .forms import LoginForm, RegistrationForm
@@ -58,4 +57,26 @@ def confirm(token):
         flash('You have confirmed your account. Thanks!')
     else:
         flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect('main.index')
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/resend_confirmation')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account', 'mail/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you email.')
     return redirect(url_for('main.index'))
